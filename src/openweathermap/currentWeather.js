f10b2e4cc5da2a3bc56c4d968ed667caedbc5e7d
zhansonthurn <12849287+zhansonthurn@users.noreply.github.com>
2022-06-02 11:23:16 -0500

    Updating error handling to more generically handle unexpected errors.

--- src/openweathermap/currentWeather.js
+++ src/openweathermap/currentWeather.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import { getLogger } from 'log4js';
-import createError from 'http-errors';
-import { v4 as uuidv4 } from 'uuid';
 
 const logger = getLogger();
 
@@ -15,20 +13,14 @@ export const getCurrentWeather = async (
   lat,
   lon,
 ) => {
-  try {
-    const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&units=imperial&appid=${process.env.OPEN_WEATHER_API_KEY}`;
-    logger.debug(`Requesting data from openweathermap weather api: ${weatherUrl}`);
-    const apiResult = await axios.get(weatherUrl);
-    logger.debug(`api result status:${(apiResult.status)} data:${JSON.stringify(apiResult.data)}`);
+  const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&units=imperial&appid=${process.env.OPEN_WEATHER_API_KEY}`;
+  logger.debug(`Requesting data from openweathermap weather api: ${weatherUrl}`);
+  const apiResult = await axios.get(weatherUrl);
+  logger.debug(`api result status:${(apiResult.status)} data:${JSON.stringify(apiResult.data)}`);
 
-    if (apiResult.status !== 200) {
-      throw new Error(`Openweathermap api call failed with status ${apiResult.status}`);
-    }
-
-    return apiResult.data;
-  } catch (error) {
-    const errorUuid = uuidv4();
-    logger.error(`Encountered unexpected error ${errorUuid}: ${error}`);
-    throw createError.InternalServerError(`Unexpected error occurred. Review logs for details of error ${errorUuid}`);
+  if (apiResult.status !== 200) {
+    throw new Error(`Openweathermap api call failed with status ${apiResult.status}`);
   }
+
+  return apiResult.data;
 };
--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,4 +1,6 @@
 import { getLogger } from 'log4js';
+import { isHttpError } from 'http-errors';
+import { v4 as uuidv4 } from 'uuid';
 
 const logger = getLogger();
 
@@ -12,10 +14,21 @@ const logger = getLogger();
 // eslint-disable-next-line no-unused-vars
 export const handleError = (error, req, res, next) => {
   logger.debug(`Processing error: ${JSON.stringify(error)}`);
+
   res.header(
     'Content-Type',
     'application/json',
   );
-  res.status(error.statusCode);
-  res.send(error);
+
+  if (isHttpError(error)) {
+    // if the error is a http error then use the status and message from the error
+    res.send(error);
+    res.status(error.statusCode);
+  } else {
+    // if the error was not a http error then log it, but return a 500 instead
+    const errorUuid = uuidv4();
+    logger.error(`Encountered unexpected error ${errorUuid}: ${error}`);
+    res.status(500);
+    res.send(JSON.stringify({ message: `Unexpected error occurred. Review logs for details of error ${errorUuid}` }));
+  }
 };
